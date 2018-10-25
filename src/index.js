ff6c0b26b787b5c9126cdbe17f2097f9646515bc
m-marangoni <marangoni.mariana@gmail.com>
2018-10-24 21:25:50 -0300

    organizando pastas

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import  Link  from './componentes/Link/Link'
-import  Botao  from './componentes/Botao/Botao';
 import './index.css'
+import  Login  from './paginas/Login/Login';
 
-
-
-const paginaLogin = (
-  <main className="pagina-login">
-    <h1>Login</h1>
-    <p>Entre com seu email e senha.</p>
-    <Link url="/conta">Criar uma conta</Link>
-    <Link url="/login">Fazer login</Link>
-    <Botao desabilitado>Enviar</Botao>
-    <Botao>Enviar</Botao>
-  </main>
-)
 
 const divisaoProjeto = document.getElementById('projeto')
-ReactDOM.render(paginaLogin, divisaoProjeto)
+const pagina = <Login/>
+ReactDOM.render(pagina, divisaoProjeto)
