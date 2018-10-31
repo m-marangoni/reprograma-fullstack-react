0cef562a3c251f04e614a3625da2fcf78b24be95
m-marangoni <marangoni.mariana@gmail.com>
2018-10-30 22:24:03 -0300

    react router + paginas novas

--- src/paginas/Contato/Contato.js
+++ src/paginas/Contato/Contato.js
@@ -2,7 +2,14 @@ import React, {Component} from 'react'
 import './Contato.css'
 
 function Contato(){
-    return
+    return(
+        <main className="contato">
+        <h1>Contato</h1>
+        <p>
+        "For those who have seen the Earth from space, and for the hundreds and perhaps thousands more who will, the experience most certainly changes your perspective. The things that we share in our world are far more valuable than those which divide us." 
+        </p>
+        </main>
+    )
 }
 
 export default Contato
