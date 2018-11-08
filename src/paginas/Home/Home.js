00fee7ffc488ad9e4fb4cba0634300783e9b82a8
m-marangoni <marangoni.mariana@gmail.com>
2018-11-07 22:26:50 -0200

    mais refatoração

--- src/paginas/Home/Home.js
+++ src/paginas/Home/Home.js
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
+import loading from './loading.svg'
 import './Home.css'
 
-function Home(props){
-    if (!props.usuario){
-        return <Redirect to="/login"/>
+
+class Home extends Component {
+    constructor(props){
+        super(props)
+        this.state = {loading: true}
     }
 
-    return ( 
-        <main className="home">
-        </main>
-    )
+    render(){
+        if (!this.props.usuario){
+            return <Redirect to="/login"/>
+        }
+    
+        return ( 
+            <main className="home">
+            {this.state.loading} ? (
+                <img className="home__loading" src={loading} alt="loading"/>
+            ) :
+            ( <div> Aqui listarei os post-its</div>)
+            </main>
+        )
+    }
 }
+
 
 export default connect(
     (state) =>  ({usuario: state.usuario})
